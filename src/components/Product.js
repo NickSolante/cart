03635aefe50e0b1e894a6e8ecad5d0fe73d66ee2
NickSolante <41457938+NickSolante@users.noreply.github.com>
2020-01-29 00:22:12 +1100

    com

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import CartContext from '../context/cartContext'
 
 const Product = () => {
-  return <h1>hello from produr</h1>
+  return <h1>hello from product</h1>
 }
 
 export default Product
