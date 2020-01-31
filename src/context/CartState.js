bdfed677ed71d79cb8f3ee62febcda0aedb472fb
NickSolante <41457938+NickSolante@users.noreply.github.com>
2020-01-31 11:27:20 +1100

    cart state update

--- src/context/CartState.js
+++ src/context/CartState.js
@@ -6,11 +6,13 @@ import { HANDLE_DETAIL, ADD_TO_CART } from '../type'
 
 const CartState = props => {
   const initialState = {
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart: []
   }
 
   const [state, dispatch] = useReducer(CartReducer, initialState)
   const [products, setProducts] = useState([])
+
   //When rather than using component did mount
   //use "useEffect hook to immitate didmount"
   useEffect(() => {
@@ -37,6 +39,13 @@ const CartState = props => {
   }
 
   const addToCart = id => {
+    let tempProducts = [...products]
+    const index = tempProducts.indexOf(getItem(id))
+    const product = tempProducts[index]
+    product.inCart = true
+    product.count = 1
+    const price = product.price
+    product.total = price
     console.log(`hello from add to cart ${id}`)
     dispatch({
       type: ADD_TO_CART,
