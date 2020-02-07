e0e8cae62b94615faee3c05fb1333ca728de4fb4
NickSolante <41457938+NickSolante@users.noreply.github.com>
2020-02-07 16:35:25 +1100

    importing dependencies for modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import styled from 'styled-components'
+import CartContext from '../context/cartContext'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Modal() {
   return <div>hello from modal</div>
