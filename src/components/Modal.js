60f135b2365b74965b2a67afb5f2c61b7587cc7c
NickSolante <41457938+NickSolante@users.noreply.github.com>
2020-02-07 16:49:04 +1100

    modal styling done

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,14 +12,18 @@ function Modal() {
         const { img, title, price } = value.modalProduct
         if (!modalOpen) {
           return null
-        } else if {
-          <ModalContainer>
-          <div className='container'>
-          <div className="row">
-            
+        } else {
+          return (
+            <ModalContainer>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize'>
+                    <h5>Item added to the cart</h5>
+                  </div>
+                </div>
               </div>
-            </div>
-          </ModalContainer>
+            </ModalContainer>
+          )
         }
       }}
     </CartContext.Consumer>
